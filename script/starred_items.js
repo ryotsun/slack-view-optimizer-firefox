c978a799d428bbb6aae9c4eeb4c15bc7a3f1ec83
ryotsun <ryo.tsugawa@gmail.com>
2017-12-10 14:48:11 +0900

    apply patch

--- script/starred_items.js
+++ script/starred_items.js
@@ -36,60 +36,69 @@ chrome.storage.sync.get(data, function(values) {
       // Starred Items
       var code_star_item = function() {
         var starred_items = {
-          init_starred_list: function() {
-            // Starred Itemsペインの開閉
-            if ($('#stars_tab').width() === 0) return;
-            var starred_items = $('#member_stars_list .message_content');
-            var l = starred_items.length;
-            //star_num_now = l;
-            for (var i = 0; i < l; i++ ) {
-              var p = document.createElement('p');
-              p.innerHTML = 'Read More...';
-              p.className = 'read_more';
+            init_starred_list: function () {
+                // Starred Itemsペインの開閉
+                if ($('#stars_tab').width() === 0) return;
+                var starred_items = $('#member_stars_list .message_content');
+                var l = starred_items.length;
+                for (var i = 0; i < l; i++) {
+                    if ($(starred_items[i]).height() > 100) {
+                        $(starred_items[i]).css({"max-height": "100px", "overflow": "hidden", "cursor": "pointer"});
 
-              if ($(starred_items[i]).height() > 100) {
-                $(starred_items[i]).css({"max-height": "100px", "overflow": "hidden", "cursor": "pointer"});
-
-                if ($(starred_items[i]).next('p').hasClass("read_more") === false) {
-                  $(starred_items[i]).parent().append(p);
-                  $(starred_items[i]).parent().parent().css({"cursor": "pointer"});
-                  $(starred_items[i]).children(".message_body").css({"cursor": "pointer"});
-                } else {
-                  $(starred_items[i]).next('.read_more').text('Read More...');
+                        if ($(starred_items[i]).next('p').hasClass("read_more") === false) {
+                            $(starred_items[i]).parent().parent().css({"cursor": "pointer"});
+                            $(starred_items[i]).children(".message_body").css({"cursor": "pointer"});
+                        } else {
+                            $(starred_items[i]).next('.read_more').text('Read More...');
+                        }
+                    }
                 }
-              }
             }
-          }
         };
 
-        TS.view.rebuildStars = function() {
-          var member = TS.model.user;
-          var html = "";
+        var starredMessages = {
+            originalStarredItemHTML: TS.templates.builders.buildStarredItemHTML,
+            originalStarredListFunction: TS.view.rebuildStars
+        };
 
-          if (TS.view.member_stars_list_lazyload && TS.view.member_stars_list_lazyload.detachEvents) {
-            TS.view.member_stars_list_lazyload.detachEvents();
-            TS.view.member_stars_list_lazyload = null;
-          }
-          if (member.stars && member.stars.length) {
-            TS.model.user.stars.forEach(function(star) {
-              html += TS.templates.builders.buildStarredItemHTML(star);
-            })
-          }
-          if (html) {
-            $("#member_stars_list").html(html);
-            starred_items.init_starred_list();
+        TS.view.rebuildStars = function(m) {
+            var originalFunction = starredMessages.originalStarredListFunction(m);
+            console.log(originalFunction);
 
-            TS.view.member_stars_list_lazyload = $("#member_stars_list").find(TS.boot_data.feature_files_list ? ".lazy" : "img.lazy").lazyload({
-              container: $("#stars_scroller")
-            });
-            TS.utility.makeSureAllLinksHaveTargets($("#member_stars_list"));
-            $("#member_stars_explanation").addClass("hidden");
-          } else {
-            $("#member_stars_list").html("");
-            $("#member_stars_explanation").removeClass("hidden")
-          }
-          TS.view.resize();
-        }
+            try {
+                originalFunction;
+                starred_items.init_starred_list();
+            } catch (e) {
+                console.info(e.stack);
+                console.info("something went wrong in starred_items.js");
+                return originalFunction;
+            }
+        };
+
+        TS.templates.builders.buildStarredItemHTML = function(m) {
+            var originalHTML = starredMessages.originalStarredItemHTML(m);
+            console.log(originalHTML);
+
+            try {
+                var target = $(originalHTML);
+
+                var html = "";
+                for(var i = 0, l = target.length; i < l; i++) {
+                    var p = document.createElement('p');
+                    p.innerHTML = 'Read More...';
+                    p.className = 'read_more';
+
+                    target[i].appendChild(p);
+                    html += target[i].outerHTML;
+                    console.log(target[i]);
+                }
+                return html;
+            } catch(e) {
+                console.info(e.stack);
+                console.info("something went wrong in starred_items.js");
+                return originalHTML;
+            }
+        };
 
         // .star_item内にhover表示のボタンがあるため、それらをクリックした際に開閉しないようにしてる
         // 気持ち悪いからなんとかしたい。
